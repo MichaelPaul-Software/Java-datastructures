b414d98102041910ad30af8ab4838bb34d50ed1d
MichaelPaul-Software <43254207+MichaelPaul-Software@users.noreply.github.com>
2018-09-26 06:31:38 -0400

    Update DoublyLinkedList.java
    3 Classes: DoublyLinkedList with all necessary methods, Node, and App

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -151,5 +151,53 @@ public void displayBackward() {
 
 
 -----------------------------------------------------------------------------------------------------------------------------
+package ds.doublylinkedlist;
 
+public class Node {
+	
+	public int data;
+	public Node next;
+	public Node previous;
+	
+	public void displayNode() {
+		System.out.print("{ " + data + " }");
+	}
+
+}
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+package ds.doublylinkedlist;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		DoublyLinkedList theList = new DoublyLinkedList();
+		
+		theList.insertFirst(22);
+		theList.insertFirst(44);
+		theList.insertFirst(66);
+		
+		theList.insertLast(11);
+		theList.insertLast(33);
+		theList.insertLast(55);
+		
+		theList.displayForward();
+		theList.displayBackward();
+		
+		theList.deleteFirst();
+		theList.deleteLast();
+		
+		theList.deleteKey(11);
+		
+		theList.insertAfter(22, 77);
+		theList.insertAfter(33, 88);
+		
+		theList.displayForward();
+		
+
+	}
+
+}
 
